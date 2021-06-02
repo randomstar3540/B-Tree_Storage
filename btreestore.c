2d89dfb0506ef60ab1e7310bb47b436369bb83eb
randomstar3540 <randomstar3540@gmail.com>
2021-06-02 15:15:23 +1000

    add

--- btreestore.c
+++ btreestore.c
@@ -930,6 +930,10 @@ int btree_delete(uint32_t key, void * helper) {
     tree_node * next_node = NULL;
     uint8_t found = FALSE;
 
+    if (key == 10202 || key == 20418){
+        fprintf(stderr,"deleted!\n");
+    }
+
 
     struct info check;
     if(btree_retrieve(key,&check,helper) == 1){
